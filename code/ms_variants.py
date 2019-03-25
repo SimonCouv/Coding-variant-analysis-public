3d66535eda24e80d648e22b59b5ca8ceaff9a4b5
Simon Couvreur G3 <simonjkcouvreur@gmail.com>
2019-03-25 16:11:55 +0100

    script and data final

--- code/ms_variants.py
+++ code/ms_variants.py
@@ -196,22 +196,19 @@ vep_coding = pickle.load( open('../data/vep_coding.p', "rb"))
 # get MAFs for coding variants
 # mrm_coding_mafs = {}
 mafs_dir = '../data/mafs_REST'
-# user_agents = get_user_agents_list()
-# for ens_id in vep_coding.keys():
-#
-#     #debug
-#     # ens_id = 'ENSG00000110244'
-#     print(ens_id)
-#
-#     mafs_fp = '{0}/{1}'.format(mafs_dir, ens_id)
-#     if os.path.isfile(mafs_fp):
-#         print(ens_id + ' already retrieved, so skipped.')
-#     else:
-#         mrm_coding_mafs = run_variant_mafs(rsids=vep_coding[ens_id].uploaded_variation.to_list(),
-#                                            get_proxies=True,
-#                                            get_user_agents=False,
-#                                            user_agents=user_agents)
-#         mrm_coding_mafs.to_csv(mafs_fp, sep='\t')
+subprocess.run("mkdir -p {0}".format(mafs_dir), shell=True, check=True)
+for ens_id in vep_coding.keys():
+
+    #debug
+    # ens_id = 'ENSG00000110244'
+    print(ens_id)
+
+    mafs_fp = '{0}/{1}'.format(mafs_dir, ens_id)
+    if os.path.isfile(mafs_fp):
+        print(ens_id + ' already retrieved, so skipped.')
+    else:
+        mrm_coding_mafs = run_variant_mafs(rsids=vep_coding[ens_id].uploaded_variation.to_list())
+        mrm_coding_mafs.to_csv(mafs_fp, sep='\t')
 
 
 
@@ -220,99 +217,110 @@ mafs_dir = '../data/mafs_REST'
 
 
 # approach:
-#   1. annotate major_allele column in mafs, calculate per rs and population
-#   2. conditional join
-#   2a. merge on rs only
-#   2b. filter on (mafs.major_allele == vep.ref_allele and mafs.pop_allele == vep.allele)
+#   1. annotate major_allele column in mafs, calculated per rs and population
+#   2. conditional join, implemented as
+#       2a. merge on rs only
+#       2b. filter on (mafs.major_allele == vep.ref_allele and mafs.pop_allele == vep.allele)
 #                   OR (mafs.major_allele == vep.allele and mafs.pop_allele == vep.ref_allele)
 
 # FIRST: check assumptions of approach:
 #   1. ref allele is among 2 most frequent alleles
 #   2. 3rd allele freq is negligible if present -> true for 99.9% of rs-pop combos
-#   extra: ref == major (no loss of information) -> true for 99% of rs-pop combos
+#   extra: ref == major (no loss of information at all) -> true for 99% of rs-pop combos
 #
-# ref_diff_major_checks = {}
-# assumption1_checks = {}
-# assumption2_checks = {}
-#
-# for ens_id in vep_coding.keys():
-#
-#     #debug
-#     # ens_id = 'ENSG00000000971'
-#
-#     print(ens_id)
-#     gene_vep_coding = vep_coding[ens_id]
-#     mafs_fp = '{0}/{1}'.format(mafs_dir, ens_id)
-#     # output of MAF REST contains alleles for rs for which at least one allele is coding in VEP
-#     gene_coding_rs_mafs = pd.read_csv(mafs_fp, sep='\t')
-#
-#     gene_assumption1_checks = {}
-#     gene_assumption2_checks = {}
-#     gene_ref_diff_major_checks = {}
-#
-#     for rs in gene_vep_coding.uploaded_variation:
-#         rs_mafs = gene_coding_rs_mafs.loc[gene_coding_rs_mafs.rs == rs]
-#         rs_vep = gene_vep_coding.loc[(gene_vep_coding.uploaded_variation == rs)]
-#
-#         rs_assumption1_checks = {}
-#         rs_assumption2_checks = {}
-#         rs_ref_diff_major_checks = {}
-#
-#
-#         for pop in rs_mafs.population:
-#             rs_pop_mafs = rs_mafs.loc[rs_mafs.population == pop]
-#             rs_pop_mafs = rs_pop_mafs.sort_values(axis=0, by='pop_allele_freq', ascending=False)
-#             rs_pop_alleles_sorted = rs_pop_mafs.pop_allele.tolist()
-#             rs_pop_allele_freqs_sorted = rs_pop_mafs.pop_allele_freq.tolist()
-#
-#             # assumption 1 check
-#             rs_ref_diff_major_checks[pop] = rs_vep.ref_allele.unique() in rs_pop_alleles_sorted[:1]
-#             rs_assumption1_checks[pop] = rs_vep.ref_allele.unique() in rs_pop_alleles_sorted[:2]
-#
-#             # assumption 2 check
-#             if len(rs_pop_alleles_sorted) < 3:
-#                 rs_assumption2_checks[pop] = True
-#             elif float(rs_pop_allele_freqs_sorted[2]) < 0.01:
-#                 rs_assumption2_checks[pop] = True
-#             else:
-#                 rs_assumption2_checks[pop] = False
-#
-#         gene_assumption1_checks[rs] = pd.Series(rs_assumption1_checks)
-#         gene_assumption2_checks[rs] = pd.Series(rs_assumption2_checks)
-#         gene_ref_diff_major_checks[rs] = pd.Series(rs_ref_diff_major_checks)
-#
-#     assumption1_checks[ens_id] = pd.concat(gene_assumption1_checks)
-#     assumption2_checks[ens_id] = pd.concat(gene_assumption2_checks)
-#     ref_diff_major_checks[ens_id] = pd.concat(gene_ref_diff_major_checks)
-#
-# assumption1_checks = pd.concat(assumption1_checks)
-# assumption2_checks = pd.concat(assumption2_checks)
-# ref_diff_major_checks = pd.concat(ref_diff_major_checks)
-#
-# assumption1_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
-# assumption2_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
-# ref_diff_major_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
-#
-# fails1 = assumption1_checks.loc[(assumption1_checks == False)]
-# fails1.to_frame().query("pop=='1000GENOMES:phase_3:EUR'").head()
-# fails1.shape
-#
-# fails2 = assumption2_checks.loc[(assumption2_checks == False)]
-# fails2.to_frame().query("pop=='1000GENOMES:phase_3:EUR'").head()
-# fails2.shape
-#
-# ref_diff_major_fails = ref_diff_major_checks.loc[(ref_diff_major_checks == False)]
-# ref_diff_major_fails.to_frame().query("pop=='1000GENOMES:phase_3:EUR'").head()
-# ref_diff_major_fails.shape
+ref_diff_major_checks = {}
+assumption1_checks = {}
+assumption2_checks = {}
 
+for ens_id in vep_coding.keys():
 
-# Now perform the back-merge and ld-expand
+    #debug
+    # ens_id = 'ENSG00000000971'
 
+    print(ens_id)
+    gene_vep_coding = vep_coding[ens_id]
+    mafs_fp = '{0}/{1}'.format(mafs_dir, ens_id)
+    # output of MAF REST contains alleles for rs for which at least one allele is coding in VEP
+    gene_coding_rs_mafs = pd.read_csv(mafs_fp, sep='\t')
+
+    gene_assumption1_checks = {}
+    gene_assumption2_checks = {}
+    gene_ref_diff_major_checks = {}
+
+    for rs in gene_vep_coding.uploaded_variation:
+        rs_mafs = gene_coding_rs_mafs.loc[gene_coding_rs_mafs.rs == rs]
+        rs_vep = gene_vep_coding.loc[(gene_vep_coding.uploaded_variation == rs)]
+
+        rs_assumption1_checks = {}
+        rs_assumption2_checks = {}
+        rs_ref_diff_major_checks = {}
+
+        for pop in rs_mafs.population:
+            rs_pop_mafs = rs_mafs.loc[rs_mafs.population == pop]
+            rs_pop_mafs = rs_pop_mafs.sort_values(axis=0, by='pop_allele_freq', ascending=False)
+            rs_pop_alleles_sorted = rs_pop_mafs.pop_allele.tolist()
+            rs_pop_allele_freqs_sorted = rs_pop_mafs.pop_allele_freq.tolist()
+
+            # assumption 1 check
+            rs_assumption1_checks[pop] = rs_vep.ref_allele.unique() in rs_pop_alleles_sorted[:2]
+
+            # assumption 2 check
+            if len(rs_pop_alleles_sorted) < 3:
+                rs_assumption2_checks[pop] = True
+            elif float(rs_pop_allele_freqs_sorted[2]) < 0.01:
+                rs_assumption2_checks[pop] = True
+            else:
+                rs_assumption2_checks[pop] = False
+
+            # check whether ref allele (VEP) is different from major allele (REST)
+            rs_ref_diff_major_checks[pop] = rs_vep.ref_allele.unique() in rs_pop_alleles_sorted[:1]
+
+        gene_assumption1_checks[rs] = pd.Series(rs_assumption1_checks)
+        gene_assumption2_checks[rs] = pd.Series(rs_assumption2_checks)
+        gene_ref_diff_major_checks[rs] = pd.Series(rs_ref_diff_major_checks)
+
+    assumption1_checks[ens_id] = pd.concat(gene_assumption1_checks)
+    assumption2_checks[ens_id] = pd.concat(gene_assumption2_checks)
+    ref_diff_major_checks[ens_id] = pd.concat(gene_ref_diff_major_checks)
+
+assumption1_checks = pd.concat(assumption1_checks)
+assumption2_checks = pd.concat(assumption2_checks)
+ref_diff_major_checks = pd.concat(ref_diff_major_checks)
+
+assumption1_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
+assumption2_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
+ref_diff_major_checks.index.set_names(['ens_id', 'rs', 'pop'], inplace=True)
+
+# save outputs, note implicit conversion from Series (multi-index) to dataframe
+assumption1_checks.to_csv('../data/assumption1_checks.tsv', sep='\t', index=True, header=True)
+assumption2_checks.to_csv('../data/assumption2_checks.tsv', sep='\t', index=True, header=True)
+ref_diff_major_checks.to_csv('../data/ref_diff_major_checks.tsv', sep='\t', index=True, header=True)
+
+assumption1_checks = pd.read_csv('../data/assumption1_checks.tsv', sep='\t')
+assumption2_checks = pd.read_csv('../data/assumption2_checks.tsv', sep='\t')
+ref_diff_major_checks = pd.read_csv('../data/ref_diff_major_checks.tsv', sep='\t')
+
+# investigate checks
+fails1 = assumption1_checks.loc[(assumption1_checks.iloc[:,3] == False)]
+fails1.query("pop=='1000GENOMES:phase_3:EUR'").head()
+fails1.shape
+
+fails2 = assumption2_checks.loc[(assumption2_checks.iloc[:,3] == False)]
+fails2.query("pop=='1000GENOMES:phase_3:EUR'").head()
+fails2.shape
+
+ref_diff_major_fails = ref_diff_major_checks.loc[(ref_diff_major_checks.iloc[:,3] == False)]
+ref_diff_major_fails.query("pop=='1000GENOMES:phase_3:EUR'").head()
+ref_diff_major_fails.shape
+
+# conclusion issues 1&2 not encountered for 1000Genomes EUR
+# some cases where ref != major allele, but these are dealt with since in
+# these cases ref is always the second most common allele in 1000Genomes EUR
+
+# Now perform the back-merge and ld-expand ---------------------------
 
 ld_partners = {}
-# ld_partners2 = {}  # to continue loop midway, after valueError due to no 1KG EUR minor alleles for gene
 vep_coding_mafs = {}
-user_agents = get_user_agents_list()
 
 for ens_id in vep_coding.keys():
 
@@ -411,9 +419,7 @@ for ens_id in vep_coding.keys():
         print("\n\nstarting ld_expand for {0} at {1}".format(ens_id, datetime.now().isoformat()),flush=True)
         ld_partners[ens_id] = run_ld_expand(rs_ids=gene_vep_coding_mafs_maxbi_eur['rs'].unique(),
                                             r2_cutoff=0.95,
-                                            window_size=500,
-                                            get_proxies=True,
-                                            user_agents=user_agents)
+                                            window_size=500)
 
 # concat over ens_ids
 ld_partners = pd.concat(ld_partners)
@@ -422,19 +428,6 @@ vep_coding_mafs = pd.concat(vep_coding_mafs)
 # export for analysis in R
 ld_partners.to_csv('../data/ld_partners', sep='\t',index=True)
 vep_coding_mafs.to_csv('../data/vep_coding_mafs', sep='\t',index=True)
-
-# EBI GWAS -------------------------------------------------------------------------------------------------------------
-
-# with open('/home/simon/OneDrive/KCL/DILI_GWAS_Molokhia/input_data/gwas-catalog-associations_ontology-annotated.tsv') as ebi_gwas_f:
-#     ebi_gwas = pd.read_csv(ebi_gwas_f, sep='\t')
-#
-# ebi_gwas.columns = (ebi_gwas.columns
-#                     .str.strip().str.lower().str.replace(' ', '_')
-#                     .str.replace('(', '').str.replace(')', ''))
-#
-# ebi_gwas >>= mask(X.snps.isin(snps_1kg_eur))
-#
-# ebi_gwas.mapped_trait.value_counts().to_csv('gwas_traits.tsv', sep='\t')
 
 
 
